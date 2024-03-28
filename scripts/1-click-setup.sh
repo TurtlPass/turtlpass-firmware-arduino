78a3aaecc249ef9a7001f98e0dcd2ab93dd2f695
Ryan Amaral <hello@ryanamaral.com>
2024-03-28 20:41:49 +0000

    remove unused aes256 and mbedtls dependecy;

--- scripts/1-click-setup.sh
+++ scripts/1-click-setup.sh
@@ -11,7 +11,6 @@ arduino-cli core update-index
 arduino-cli core install --additional-urls https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json rp2040:rp2040
 arduino-cli config set library.enable_unsafe_install true
 arduino-cli lib install --git-url git@github.com:rweather/arduinolibs.git
-arduino-cli lib install --git-url git@github.com:Seeed-Studio/Seeed_Arduino_mbedtls.git
 arduino-cli lib install --git-url git@github.com:dirkx/Arduino-Base32-Decode.git
 arduino-cli lib install --git-url git@github.com:FrankBoesing/FastCRC.git
 arduino-cli lib install --git-url git@github.com:dojyorin/arduino_base64.git
